672207cd3a78597b2ca5836b53feed8c7880ee95
Matthew Scott <matt@11craft.com>
2016-11-29 13:49:45 -0600

    Embed HTML5 audio player above WAV download links

---
+++ docs/_static/listen_to_wav_links.js
@@ -0,0 +1,4 @@
+$('a[href$="wav"]').each(function() {
+    var audioURL = $(this).attr('href');
+	$(this).before('<audio controls src="'+audioURL+'" type="audio/wav" preload="none"></audio><br/>');
+});
