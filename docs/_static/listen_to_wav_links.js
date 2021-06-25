87894fa2b4e19393148058f1a59bbf5ad4fd353f
Matthew Scott <matt@11craft.com>
2021-06-25 00:35:12 -0500

    Renders audio tags after page load completes

--- docs/_static/listen_to_wav_links.js
+++ docs/_static/listen_to_wav_links.js
@@ -1,4 +1,4 @@
-$('a[href$="wav"]').each(function() {
+$(function() { $('a[href$="wav"]').each(function() {
     var audioURL = $(this).attr('href');
 	$(this).before('<audio controls src="'+audioURL+'" type="audio/wav" preload="none"></audio><br/>');
-});
+})});
